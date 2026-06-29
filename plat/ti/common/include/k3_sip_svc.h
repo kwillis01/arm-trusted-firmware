28b41e16b8dca046faab942f562c54d8d0e32ebc
Kendall Willis <k-willis@ti.com>
2026-06-29 18:53:00 -0500

    PENDING: feat(ti): Add SMC call to expose LPM and wakeup info
    Add K3_SIP_GET_WKUP_REASON SiP SMC call so Linux can query which low power
    mode was last entered, the wake source triggered resume, and the pin
    that caused the wakeup if applicable.
    
    Change-Id: Ibda310cccefb9e50b34e1897473d52b90fbd52a1
    Signed-off-by: Kendall Willis <k-willis@ti.com>

--- plat/ti/common/include/k3_sip_svc.h
+++ plat/ti/common/include/k3_sip_svc.h
@@ -17,13 +17,14 @@
 #define K3_SIP_OTP_WRITEBUFF    0xC2000000
 #define K3_SIP_OTP_WRITE        0xC2000001
 #define K3_SIP_OTP_READ         0xC2000002
+#define K3_SIP_GET_WKUP_REASON  0xC2000003
 
 /* TI SiP Service Calls version numbers */
 #define K3_SIP_SVC_VERSION_MAJOR	0x0
 #define K3_SIP_SVC_VERSION_MINOR	0x1
 
 /* Number of TI SiP Calls implemented */
-#define K3_COMMON_SIP_NUM_CALLS     0x6
+#define K3_COMMON_SIP_NUM_CALLS     0x7
 
 /* TI Fuse writebuff SMC handler */
 int ti_fuse_writebuff_handler(u_register_t x1);
